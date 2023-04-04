9c00d43ca36de8653495e546ee5f3175745de43f
Pedro Sousa Coelho <pedro.coelho@solutinet.com.br>
2023-04-04 14:56:26 -0300

    3

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,14 +1,18 @@
 class NegociacaoController{ 
+
+    constructor(){ 
+
+        let $ = document.querySelector.bind(document)
+
+        this._inputData = $("#data")
+        this._inputQuantidade = $("#quantidade")
+        this._inputValor = $("#valor")
+
+    }
+
     adiciona(event){
         event.preventDefault()
         
-        let inputData = document.querySelector("#data")
-        let inputQuantidade = document.querySelector("#quantidade")
-        let inputValor = document.querySelector("#valor")
-
-        console.log(inputData.value)
-        console.log(inputQuantidade.value)
-        console.log(inputValor.value)
-        
+        let data = new Date(this._inputData.value.split('-'))
     }
 }
